87b2317fa839ea50115bda9bc3e2a0af3e2f35ef
jeremy <jeremy.nowak@laplateforme.io>
2023-09-12 13:27:40 +0200

    feature/abstractClass/connexion/OK

--- Database.php
+++ Database.php
@@ -1,17 +1,38 @@
 
 <?php
 abstract class Database{
+    protected $bdd;
+    public $host;
+    public $dbname;
+    public $dbUser;
+    public $dbPass;
 
+    public function __construct(){
 
-    private $pdo;
-
-    public function __construct($pdo){
         
-    $this->pdo = new PDO('mysql:host=localhost;dbname=moduleconnexionb2;charset=utf8', 'root', '');
+        $this->host = 'localhost';
+        $this->dbname = 'moduleconnexionb2';
+        $this->dbUser = 'root';
+        $this->dbPass = '';
 
+        try {
+            $this->bdd = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->dbUser, $this->dbPass);
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->bdd->exec("set names utf8");
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+            die();
+        }
     }
 
-}
+        
+
+        // $pdo = new PDO('mysql:host=localhost;dbname=moduleconnexionb2;charset=utf8', 'root', '');
+        // $this->bdd = $pdo;   
+        // return $this->bdd;
+    }
+
+
 
 
 ?>
--- User.php
+++ User.php
@@ -1,17 +1,17 @@
 
 <?php
-
 require_once "Database.php";
 
-class User{
+class User extends Database{
 
-
+// pro $bdd;
 private $login;
 private $firstname;
 private $lastname;
 
     public function __construct( $login = null, $firstname = null, $lastname = null) {
 
+        parent::__construct();
 
     if (isset($_SESSION["user"])) {
         
@@ -26,10 +26,8 @@ private $lastname;
 
         public function checkLogin($login){
 
-    $pdo = new PDO('mysql:host=localhost;dbname=moduleconnexionb2;charset=utf8', 'root', '');
     $sql = "SELECT login FROM user WHERE login = :login";
-    
-    $statement = $pdo->prepare($sql);
+    $statement = $this->bdd->prepare($sql);
     $statement->execute([':login' => $login]);
     $student = $statement->fetch(PDO::FETCH_ASSOC);
 
